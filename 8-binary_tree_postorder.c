0c9b9de2f8ff0c765a6c0351e73a8f0438e8cc66
LanMalJR <lanmaljrlm>
2024-04-11 12:03:28 -0700

    white space

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - a function that goes through a binary tree 
+ * binary_tree_postorder - a function that goes through a binary tree
  * using post-order traversal.
  * @tree: Is a pointer to the root node of the tree to traverse.
  * @func: Is a pointer to a function to call for each node.
